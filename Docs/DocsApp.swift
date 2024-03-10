74b5e56304758b39adf09444c3d90142b88d9bc7
@Slava-Fly <school.94@bk.ru>
2024-03-10 15:32:43 +0800

    Initial Commit

---
+++ Docs/DocsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DocsApp.swift
+//  Docs
+//
+//  Created by User on 10.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct DocsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
